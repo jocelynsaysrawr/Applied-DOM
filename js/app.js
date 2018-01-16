afa34e16ec732d4f1a09d83bb922a92a12551954
Jocelyn Belden <jocelynbelden@Jocelyns-MacBook-Pro.local>
2018-01-15 22:50:32 -0800

    btn10 turned green using bind and setMyLightGreen

--- js/app.js
+++ js/app.js
@@ -179,6 +179,7 @@
    * to set the context to the btn10 object
    */
 
+   const setLight10Green = setMyLightGreen.bind(btn10);
 
   /*
    * Add a click event listener to btn10
@@ -187,6 +188,9 @@
    * will invoke the setLight10Green function
    */
 
+   btn10.addEventListener('click', function() {
+     setLight10Green();
+   });
 
   /*
    * Declare a new const named setLight11Green
