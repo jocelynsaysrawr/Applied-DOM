54e4c6c606c1c60818d74968ec1e2eabf3e46aaf
Jocelyn Belden <jocelynbelden@Jocelyns-MacBook-Pro.local>
2018-01-15 22:53:07 -0800

    btn11 turned green using bind and setMyLightClass

--- js/app.js
+++ js/app.js
@@ -201,6 +201,7 @@
    *     null, 'light-green'
    */
 
+   const setLight11Green = setMyLightClass.bind(btn11, null, 'light-green');
 
   /*
    * Add a click event listener to btn11
@@ -209,6 +210,9 @@
    * will invoke the setLight11Green function
    */
 
+  btn11.addEventListener('click', function() {
+    setLight11Green();
+  });
 
   /*
    * Declare a new const named setLight12Class
