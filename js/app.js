845c232c8f70fc1d6dcdeaa2b700a14ae674cbd4
Jocelyn Belden <jocelynbelden@Jocelyns-MacBook-Pro.local>
2018-01-15 23:00:35 -0800

    btn13 turned green using bind, this, and setMyLightClass

--- js/app.js
+++ js/app.js
@@ -251,6 +251,9 @@
    *     event, 'light-green'
    */
 
+   btn13.addEventListener('click', function(event) {
+     setMyLightClass.bind(this)(event, 'light-green');
+   })
 
   /*
    * Add a click event listener to btn14
